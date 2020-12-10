6df36fbed56de588c3ee28816eb319aef26b6ff9
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2020-12-10 11:09:09 +0000

    Change backup files behaviour

--- configs/editor/emacs.el
+++ configs/editor/emacs.el
@@ -8,8 +8,14 @@
       line-number-mode t
       scroll-bar-mode -1
       shift-select-mode t
-      )
 
+      backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying t    ; Don't delink hardlinks
+      version-control t      ; Use version numbers on backups
+      delete-old-versions t  ; Automatically delete excess backups
+      kept-new-versions 20   ; how many of the newest versions to keep
+      kept-old-versions 5    ; and how many of the old
+      )
 ;; Setting this variable directly does not take effect;
 ;; either customize it (see the info node ‘Easy Customization’)
 ;; or call the function ‘menu-bar-mode’.
@@ -20,7 +26,10 @@
 ;; or call the function ‘tool-bar-mode’.
 (tool-bar-mode -1)
 
-
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-c b <up>") 'windmove-up)
+(global-set-key (kbd "C-c b <down>") 'windmove-down)
+(global-set-key (kbd "C-c b <left>") 'windmove-left)
+(global-set-key (kbd "C-c b <right>") 'windmove-right)
