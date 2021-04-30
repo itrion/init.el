e14849125f4becac0de554cd51df5b8abe976279
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2021-04-30 12:14:14 +0100

    move key bindings to a dedicated file

---
+++ configs/editor/keybindings.el
@@ -0,0 +1,13 @@
+(global-set-key (kbd "s-k") 'kill-this-buffer)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(global-set-key (kbd "C-c b <up>") 'windmove-up)
+(global-set-key (kbd "C-c b <down>") 'windmove-down)
+(global-set-key (kbd "C-c b <left>") 'windmove-left)
+(global-set-key (kbd "C-c b <right>") 'windmove-right)
+
+(global-set-key (kbd "C-c o u") 'org-update-checkbox-count)
+
+(global-set-key (kbd "C-c t l") 'toggle-truncate-lines)
+
+(global-set-key (kbd "C-c .") 'completion-at-point)
