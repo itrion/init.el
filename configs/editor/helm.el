b65b723b31367df47be640624b0ac5d09256b203
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2021-04-30 12:36:15 +0100

    move helm keybinding

--- configs/editor/helm.el
+++ configs/editor/helm.el
@@ -18,21 +18,4 @@
       helm-apropos-fuzzy-match              t
       )
 
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ;  make TAB work in terminal
-(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-set-key (kbd "C-c h o") 'helm-occur)
-
-(global-set-key (kbd "C-h b") 'helm-descbinds)
-
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-c b b") 'helm-mini)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-
-
 (helm-autoresize-mode 1)
