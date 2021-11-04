f580a9f9cd9ebbfd61cf8f4a1ae7ad25cb748b8b
Johan Sebastian Cortes Montenegro <johan@theagilemonkeys.com>
2021-11-04 10:51:49 +0000

    add some defaults for org-mode

--- configs/editor/org.el
+++ configs/editor/org.el
@@ -1,2 +1,10 @@
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(setq org-log-into-drawer "LOGBOOK"
+      org-hide-emphasis-markers t
+      org-hide-block-startup t
+      org-hide-leading-stars t
+      org-startup-indented t
+      org-startup-folded t
+      )
