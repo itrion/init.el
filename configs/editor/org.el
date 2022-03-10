33c95693bce4a4e3ffa33e04cb34dc703b6f7ad7
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2022-03-10 11:47:20 +0000

    enable babel and adjust some org configs

--- configs/editor/org.el
+++ configs/editor/org.el
@@ -1,12 +1,16 @@
 (require 'org-bullets)
 (require 'org-journal)
-
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(add-hook 'org-mode-hook (lambda () (progn (org-bullets-mode 1)
+					   (display-line-numbers-mode 0))))
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((shell . t)
+   ))
 
 (setq org-log-into-drawer "LOGBOOK"
+      org-hide-leading-stars t
       org-hide-emphasis-markers t
       org-hide-block-startup t
-      org-hide-leading-stars t
       org-startup-indented t
       org-startup-folded t
 
