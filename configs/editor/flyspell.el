921f1b9888d511b48024f5bc608a3ac2886e5f5a
Johan Sebastian Cortes Montenegro <johan@theagilemonkeys.com>
2022-03-13 22:46:01 +0000

    fix issue with flyspell and git commits

--- configs/editor/flyspell.el
+++ configs/editor/flyspell.el
@@ -9,6 +9,8 @@
 
 (add-hook 'text-mode-hook 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+;; https://emacs.stackexchange.com/questions/41403/git-commit-mode-with-flyspell-very-slow
+(add-hook 'git-commit-setup-hook 'git-commit-turn-on-flyspell)
 
 (add-hook 'flyspell-mode-hook (lambda () (auto-dictionary-mode 1)))
 ;;; flyspell.el ends here
